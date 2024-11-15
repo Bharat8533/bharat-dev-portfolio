b71ad3291d405680a47d3f561521bb6ea78f4df4
bharat <bharatsharma853399@gmail.com>
2024-11-15 11:23:33 +0530

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -23,11 +23,11 @@ function Header() {
 
   return (
     <nav className="w-full border-b border-gray-200" >
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto h-16 p-4">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto h-16">
         <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
-          <span className="logo self-center text-2xl font-semibold whitespace-nowrap tracking-tighter">Bharat</span>
+          <span className="logo self-center text-2xl font-semibold whitespace-nowrap tracking-tighter p-2 pt-4">Bharat</span>
         </Link>
-        <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+        <div className="flex pt-4 p-2 items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
           <Link to={'https://wa.me/917417216376?text=Hello%2C%20test'} className="text-white w-8 sm:w-10 h-8 sm:h-10 bg-gray-800 hover:bg-purple-900 focus:outline-none font-medium rounded-full flex items-center justify-center text-xl sm:text-2xl ">
             <FaWhatsapp />
           </Link>
@@ -44,7 +44,7 @@ function Header() {
         </div>
         <div className={`items-center justify-between ${display === true ? 'block' : 'hidden' } w-full h-full md:flex md:w-auto md:order-1 md:mt-0`} style={{zIndex: '1200'}} id="navbar-sticky">
           <ul className="flex flex-col items-center
-           md:p-0 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:border-0 md:bg-transparent h-fit">
+           md:p-0 font-medium border border-gray-100 bg-white md:space-x-8 rtl:space-x-reverse md:flex-row md:border-0 md:bg-transparent h-fit w-full">
             {
               links.map((link, index) => {
                 return (
